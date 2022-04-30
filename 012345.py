9df9fb338ef3059f0bfce067cef3668faa9a271d
Mux <96343394+muxzv@users.noreply.github.com>
2022-04-30 18:15:56 +0300

    Add files via upload

---
+++ 012345.py
@@ -0,0 +1,66 @@
+print('     _  ')
+print('  /     \  ')
+print(' |      |')
+print('  \  _ /')
+
+
+print()
+
+
+
+print('        _')
+print('     /   |')
+print('   / /| |')
+print('/_/ |  |')
+print('      |  |')
+print('      |_|')
+
+
+print()
+
+
+
+print('  /------\ ')
+print(' |        |')
+print('          /')
+print('        /')
+print('     /___')
+
+
+print()
+
+
+
+print('    /  --  \ ')
+print(' /        /')
+print('         / ')
+print('         \ ')
+print(' \         \ ')
+print('    \   _  /  ')
+
+
+print()
+
+
+
+print('  /        |')
+print(' |____ |')
+print('            |')
+print('            |')
+
+
+print()
+
+
+
+print('  -------- ')
+print(' |')
+print(' |')
+print('  ------\ ')
+print('          |')
+print('____/')
+
+
+print()
+print()
+
