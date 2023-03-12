589cd27256caff44ae3f61e6feb28c050f481fab
Mux <96343394+muxzv@users.noreply.github.com>
2023-03-12 12:46:40 +0300

    Update TicTacToe.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,8 +1,12 @@
 #Крестики-Нолики
 
-field = ['1','2', '3', '4', '5', '6', '7', '8', '9']
+field = ['1', '2', '3',
+         '4', '5', '6',
+         '7', '8', '9']
 num = 2
 win_indexes = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+cursym = 'X'
+win = 0
 
 def correct_num(num, field):
     return not(field[num] == 'X' or field[num] == 'O' or num < 0 or num > 8)
@@ -10,46 +14,74 @@ def correct_num(num, field):
 def paintP(num, symbol, field): #Функция для закраски клетки в поле
     field[num - 1] = symbol
 
+def allF(field):
+     for l in field:
+          for j in l:
+               if not(j == 'X' or j == 'O'):
+                    return False
+     return True
+
 def winF(field, symbol): # Функция для проверки выигрыша
     #По диагонали
     for i in win_indexes:
         if field[i[0]]==symbol and field[i[1]]==symbol and field[i[2]]==symbol:
             return True
     return False
-    
-def prntFi(f):
+
+def prntFi(field):
    for i in range(0,9,3):
       print(field[i], field[i + 1], field[i + 2])
 
 while True:
-    prntFi(field)
+     prntFi(field)
 
-    #Ввод координат крестиков
-    print('Ход крестиков!')
-    while True:
-        num = int(input('Введите номер клетки: '))
-        if correct_num(num-1, field):
-            break
-    #Ход
-    paintP(num, 'X', field)
-    if winF(field, 'X'):
-        print('Крестики победили!')
-        prntFi(field)
-        break
-    
-    prntFi(field)
-    
-   #Ввод координат ноликов
-    print('Ход ноликов!')
-    while True:
-        num = int(input('Введите номер клетки: '))
-        if correct_num(num-1, field):
-            break
-    #Ход
-    paintP(num, 'O', field)
-    if winF(field, 'O'):
-        print('Нолики победили!')
-        prntFi(field)
-        break
+     #Проверка ничьи
+     if allF(field):
+          print('Ничья!')
+          break
+     
+     #Ввод координат
+     if cursym == 'X':
+          print('Ход крестиков!')
+     else:
+          print('Ход ноликов!')
+     
+     while True:
+          num = input('Введите номер клетки: ')
+          #Проверка на сдачу '?'
+          if num == 'сдаюсь':
+               if cursym == 'X':
+                     print('Нолики победили!')
+                     win = 1
+                     break
+               else:
+                    print('Крестики победили!')
+                    win = 1
+                    break
+          
+          if correct_num(int(num) - 1, field):
+               break
 
-#TODO: удалить копи паст
+     if win == 1:
+          break
+
+     paintP(int(num), cursym, field)
+     
+     #Проверка выигрыша 
+     if winF(field, cursym):
+          if cursym == 'X':
+               print('Крестики победили!')
+               prntFi(field)
+               break
+          else:
+               print('Нолики победили!')
+               prntFi(field)
+               break
+     
+     if cursym == 'X':
+          cursym = 'O'
+     else:
+          cursym = 'X'
+    
+
+#TODO: что нибудь сделать
