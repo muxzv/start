221017729f86b5d52bbe7f666d7064c1d1c5a2e4
Mux <96343394+muxzv@users.noreply.github.com>
2024-01-21 21:09:30 +0300

    Add files via upload

--- arduino/bat_thingy.a.ino
+++ arduino/bat_thingy.a.ino
@@ -1,1 +1,22 @@
+#define PIN_TRIG 12
+#define PIN_ECHO 11
+long duration, cm;
 
+void setup() {
+  Serial.begin(9600);
+  pinMode(PIN_TRIG, OUTPUT);
+  pinMode(PIN_ECHO, INPUT);
+}
+void loop() {
+  digitalWrite(PIN_TRIG, LOW);
+  delayMicroseconds(5);
+  digitalWrite(PIN_TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(PIN_TRIG,LOW);
+  duration = pulseIn(PIN_ECHO,HIGH);
+  cm=(duration / 2) / 29.1;
+  Serial.print("Расстояние до объекта:");
+  Serial.print(cm);
+  Serial.println(" см.");
+  delay(250);
+}
