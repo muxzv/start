81f6e3c278453de478d697bd24fe24416924c8b4
Mux <96343394+muxzv@users.noreply.github.com>
2024-03-16 18:08:32 +0300

    Update fractions.py

--- fractions.py
+++ fractions.py
@@ -13,6 +13,11 @@ def sokr(d):
     return d[0] // n, d[1] // n
 
 def add(d1, d2):
-    return d1
-    
+    n = nok(d1[1], d2[1])
+    n1 = n // d1[1]
+    n2 = n // d2[1]
+    delitel1 = n // d1[1]
+    delitel2 = n // d2[1]
+    return sokr((d1[0] * delitel1 + d2[0] * delitel2, n))
+
 print(add((3,12),(1,3)))
