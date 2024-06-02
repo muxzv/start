cc59f2643ec0f4b346dd9d21679de30ad4f2032c
cooffice <55910898+cooffice@users.noreply.github.com>
2024-06-02 13:16:59 +0300

    Update sand_clocks.py

--- sand_clocks.py
+++ sand_clocks.py
@@ -19,33 +19,27 @@ while len(l) > 0:
     time_before, a, b, path = state[0], state[1], state[2], state[3]
     if a > 0 and b > 0:
         if a > b:
-            a, b = a-b, 0
-            time_before += b
             path += ' t(' + str(b) + ')'
+            time_before += b
+            a, b = a-b, 0
         else:
-            a, b = 0, b-a
-            time_before += a
             path += ' t(' + str(a) + ')'
+            time_before += a
+            a, b = 0, b-a
     elif a > 0:
-        time_before += a
         path += ' t(' + str(a) + ')'
+        time_before += a
         a = 0
     elif b > 0:
-        time_before += b
         path += ' t(' + str(b) + ')'
+        time_before += b
         b = 0
-    #else:
-        #a, b = max_a, max_b
-    print(time_before, a, b, path)
     if time_before > max_time:
         continue
-    for action in 'w','a','b','ab':
-        if action == 'w' and (a>0 or b>0):
-            l.append([time_before, a, b, path + ' ' + action])
-        if 'a' in action:
-            l.append([time_before, max_a - a, b, path + ' ' + action])
-        if 'b' in action:
-            l.append([time_before, a, max_b - b, path + ' ' + action])
+    print(time_before, a, b, path)
+    l.append([time_before, max_a - a, b, path + ' a'])
+    l.append([time_before, a, max_b - b, path + ' b'])
+    l.append([time_before, max_a - a, max_b - b, path + ' ab'])
     i += 1
     if i > 100:
         break
