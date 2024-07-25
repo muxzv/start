74b2cece98ef96dc25f136ea37068d43ad8864ce
cooffice <55910898+cooffice@users.noreply.github.com>
2024-07-25 09:59:58 +0300

    Update ChangeAnimations.lua

--- roblox/ChangeAnimations.lua
+++ roblox/ChangeAnimations.lua
@@ -1,11 +1,32 @@
 -- LocalScript under Character
 
-local runAnimation = "rbxassetid://18618439306" -- replace these with your own animation ids
-local jumpAnimation = "rbxassetid://18618443642" -- replace these with your own animation ids
+local runAnimationId = "rbxassetid://18618439306" -- replace these with your own animation ids
+local jumpAnimationId = "rbxassetid://18618443642" -- replace these with your own animation ids
+
+local runAnimation = Instance.new("Animation")
+runAnimation.AnimationId = runAnimationId
+
+local jumpAnimation = Instance.new("Animation")
+jumpAnimation.AnimationId = jumpAnimationId
+
+local assets = {
+	runAnimation,
+	jumpAnimation,
+}
+
+local ContentProvider = game:GetService("ContentProvider")
+
+-- This will be hit as each asset resolves
+local callback = function(assetId, assetFetchStatus)
+  print("PreloadAsync() resolved asset ID:", assetId)
+  print("PreloadAsync() final AssetFetchStatus:", assetFetchStatus)
+end
+
+ContentProvider:PreloadAsync(assets, callback)
 
 local character = script.Parent
 local animateScript = character:WaitForChild("Animate")
 
-animateScript.run.RunAnim.AnimationId = runAnimation
-animateScript.walk.WalkAnim.AnimationId = runAnimation
-animateScript.jump.JumpAnim.AnimationId = jumpAnimation
+animateScript.run.RunAnim.AnimationId = runAnimationId
+animateScript.walk.WalkAnim.AnimationId = runAnimationId
+animateScript.jump.JumpAnim.AnimationId = jumpAnimationId
