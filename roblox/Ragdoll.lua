cfb87ca21eb7f3388091604e4a9cbd694238369d
Mux <96343394+muxzv@users.noreply.github.com>
2024-07-28 11:44:29 +0300

    Create Ragdoll.lua

---
+++ roblox/Ragdoll.lua
@@ -0,0 +1,22 @@
+local Humanoid = script.Parent:WaitForChild("Humanoid")
+Humanoid.BreakJointsOnDeath = false
+
+Humanoid.Died:Connect(function()
+	for index, joint in pairs(script.Parent:GetDescendants()) do
+		if joint:IsA("Motor6D") then
+			local socket = Instance.new("BallSocketConstraint")
+			local a1 = Instance.new("Attachment")
+			local a2 = Instance.new("Attachment")
+			a1.Parent = joint.Part0
+			a2.Parent = joint.Part1
+			socket.Parent = joint.Parent
+			socket.Attachment0 = a1
+			socket.Attachment1 = a2
+			a1.CFrame = joint.C0
+			a2.CFrame = joint.C1
+			socket.LimitsEnabled = true
+			socket.TwistLimitsEnabled = true
+			joint:Destroy()
+		end
+	end
+end)
