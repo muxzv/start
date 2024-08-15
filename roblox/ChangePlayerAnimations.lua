fb12d62109cc5b201d141c5bae991e86218ef3a2
Mux <96343394+muxzv@users.noreply.github.com>
2024-08-15 11:56:01 +0300

    Update ChangePlayerAnimations.lua

--- roblox/ChangePlayerAnimations.lua
+++ roblox/ChangePlayerAnimations.lua
@@ -5,10 +5,12 @@ local jumpAnimationId = "rbxassetid://18834471405" -- replace these with your ow
 local toolslashAnimationId = "rbxassetid://18826882897" -- replace these with your own animation ids
 local idleAnimationId = "rbxassetid://18834613915" -- replace these with your own animation ids
 
+local NOHANDOUT_ID = 04484494845 -- не менять
+
 local function DisableHandOut(character)
   local Animator = character.Animate
   local Animation = Instance.new("Animation")
-  Animation.AnimationId = idleAnimationId
+  Animation.AnimationId = "http://www.roblox.com/asset/?id="..NOHANDOUT_ID
 
   local ToolNone = Animator:FindFirstChild("toolnone")
   if ToolNone then
