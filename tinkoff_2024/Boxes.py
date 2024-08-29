8e420a56a92961e51607ca897bce3c053e738f0f
Mux <96343394+muxzv@users.noreply.github.com>
2024-08-29 12:02:40 +0300

    Update Boxes.py

--- tinkoff_2024/Boxes.py
+++ tinkoff_2024/Boxes.py
@@ -9,8 +9,9 @@ def fit(a,b,c,d,e,f):
     '''
     return a <= d and b <= e and c <= f
 
-f12 = fit(a,b,c,d,e,f)
-f21 = fit(d,e,f,a,b,c)
+f12 = fit(a,b,c,d,e,f) or fit(a,b,c,e,f,d) or fit(a,b,c,f,d,e) or fit(b,c,a,d,e,f) or fit(b,c,a,e,f,d) or fit(b,c,a,f,d,e) or fit(c,a,b,d,e,f) or fit(c,a,b,e,f,d) or fit(c,a,b,f,d,e)
+
+f21 = fit(d,e,f,a,b,c) or fit(d,e,f,b,c,a) or fit(d,e,f,c,a,b) or fit(e,f,d,a,b,c) or fit(e,f,d,b,c,a) or fit(e,f,d,c,a,b) or fit(f,d,e,a,b,c) or fit(f,d,e,b,c,a) or fit(f,d,e,c,a,b)
 
 if f12 and f21:
     print("Equal") #равны
