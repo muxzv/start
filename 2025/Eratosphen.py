96442376b520446db193168ee6d2963160615567
Mux <96343394+muxzv@users.noreply.github.com>
2025-02-21 18:18:24 +0300

    Update Eratosphen.py

--- 2025/Eratosphen.py
+++ 2025/Eratosphen.py
@@ -1,23 +1,23 @@
 def task_eratosphen(a):
   
-  nums_dict = [True for i in range(a+1)]
+  nums_list = [True for i in range(a+1)]
   
   sqr_a = a ** 0.5
   
-  res = []
+  simple_set = set()
   
   for i in range(2, a+1):
     if i > sqr_a:
       break
-    if nums_dict[i]:
-      res.append(i)
+    if nums_list[i]:
+      simple_set.add(i)
     for j in range(i*i, a+1, i):
-      nums_dict[j] = False
+      nums_list[j] = False
     
   for i in range(i, a+1):
-    if nums_dict[i]:
-      res.append(i)
+    if nums_list[i]:
+      simple_set.add(i)
   
-  return res
+  return simple_set
 
 print(task_eratosphen(100))
