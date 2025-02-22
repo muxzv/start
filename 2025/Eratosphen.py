ac77bf530a81d1f78c7b9e9d72f4d82b7239d019
Mux <96343394+muxzv@users.noreply.github.com>
2025-02-22 09:33:02 +0300

    Update Eratosphen.py

--- 2025/Eratosphen.py
+++ 2025/Eratosphen.py
@@ -1,3 +1,15 @@
+def visulize(num_list, i):
+  if i < 10:
+    res_list = " ";
+  res_list += str(i) + " "
+
+  for e in num_list[2:]:
+    if e:
+      res_list += 'o'
+    else:
+      res_list += 'x'
+  print(res_list)
+
 def task_eratosphen(a):
   
   nums_list = [True for i in range(a+1)]
@@ -9,7 +21,7 @@ def task_eratosphen(a):
   simple_set = set()
   
   for i in range(2, a+1):
-    if i > sqr_a:
+    if i**2 > a:
       break
     if nums_list[i]:
       simple_set.add(i)
@@ -17,14 +29,15 @@ def task_eratosphen(a):
       nums_list[j] = False
       if not j in d:
         d[j] = i
-    
+    visulize(nums_list, i)
+
   for i in range(i, a+1):
     if nums_list[i]:
       simple_set.add(i)
-  
+
   return simple_set,d
 
-simples, divides = task_eratosphen(100)
+simples, divides = task_eratosphen(20)
 
 print('simple:', simples)
 print()
