f2bff82d1e7b7e96941960a70a9d521e92de1b67
Mux <96343394+muxzv@users.noreply.github.com>
2025-02-28 11:52:30 +0300

    Update AI2.py

--- AI2_Project/AI2.py
+++ AI2_Project/AI2.py
@@ -1,14 +1,41 @@
-def SplitText(s):
-  return ["1", "2"]
+import json
+import requests
+import pymssql
 
-def FindComponentBySyn(s):
-  return 123 if s == "1" else 888 if s == "3" else None
+PROMPT = "The input text lists the components of cosmetic product separated by commas. For each, find the most suitable match in INCI (International Nomenclature of Cosmetic Ingredients) and display semicolon-separated list like: source component 1 -> INCI ingredient code 1; source component 2 -> INCI ingredient code 2. Some composite components may contain multiple INCI-components (sample: шаромикс 705 -> Benzoic Acid, Sorbic Acid, Dehydroacetic Acid, Benzyl alcohol)"
+
+def SplitText(s):
+  s1 = s.split(',')
+  return s1
+
+def SqlServerConnect():
+  return pymssql.connect(server,user,password,database='InciParserDb',as_dict=True)
 
 def FindComponentByInci(s):
-  pass
+  sql = "select [IdComponent] from [tbComponent] where [Inci]='" + s + "'"
+  conn = SqlServerConnect()
+  cursor = conn.cursor()
+  cursor.execute(sql)
+  row = cursor.fetchone()
+  id = row[0]
+  conn.close()
+  return id
+
+def FindComponentBySyn(s):
+  sql = "select [IdComponent] from [tbComponentSyn] where [Title]='" + s + "'"
+  conn = SqlServerConnect()
+  cursor = conn.cursor()
+  cursor.execute(sql)
+  row = cursor.fetchone()
+  id = row[0]
+  conn.close()
+  return id
 
 def AskLLM(s):
-  return "3"
+  url = "https://localhost:17555/llmstudio?prompt=" + PROMPT + "&request=" + s
+  response = requests.get(url)
+  s = json.loads(response)["response"]
+  return s.split(" -> ")[1]
 
 def MainParsingTask(st):
   ResList = []
