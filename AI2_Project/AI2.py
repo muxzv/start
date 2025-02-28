b25005dba9f9adea1b19f51849bdea162d1bf71b
Mux <96343394+muxzv@users.noreply.github.com>
2025-02-28 09:03:40 +0300

    Update AI2.py

--- AI2_Project/AI2.py
+++ AI2_Project/AI2.py
@@ -1,14 +1,30 @@
 def SplitText(s):
+  return ["1", "2"]
+
+def FindComponentBySyn(s):
+  return 123 if s == "1" else 888 if s == "3" else None
+
+def FindComponentByInci(s):
   pass
 
-def FindComponentSyn(s):
-  pass
+def AskLLM(s):
+  return "3"
 
-st = input()
-
-ResList = []
-
-Lst = SplitText(st)
-
-for s in Lst:
+def MainParsingTask(st):
+  ResList = []
   
+  Lst = SplitText(st)
+  
+  for s in Lst:
+    IdComponent = FindComponentBySyn(s)
+    if IdComponent == None:
+      inci = AskLLM(s)
+      if inci != None:
+        IdComponent = FindComponentByInci(inci)
+        if IdComponent == None:
+          IdComponent = FindComponentBySyn(inci)
+    ResList.append(IdComponent)
+    
+  return ResList
+    
+print(MainParsingTask("1,2"))
