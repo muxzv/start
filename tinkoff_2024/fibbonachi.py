39454086fd00a74bc81f49b51768fe5c70194824
Mux <96343394+muxzv@users.noreply.github.com>
2025-06-20 15:39:50 +0300

    Update fibbonachi.py

--- tinkoff_2024/fibbonachi.py
+++ tinkoff_2024/fibbonachi.py
@@ -1,5 +1,14 @@
 from functools import lru_cache
 
+def fibanan_dp(n): # методом динамического программирования
+  dp = [0] * n
+  dp[0] = 1
+  dp[1] = 1
+  for i in range(2, n):
+    dp[i] = dp[i-1] + dp[i-2]
+  return dp[n-1]
+
+
 @lru_cache() # включает кэширование, то есть сохраняет и использует ранее полученнее значения функции
 def fibanan2(n):
   global c
@@ -43,7 +52,8 @@ c = 0
 print(fibanan2(11))
 print('Кол-во вызовов с кэшированием:', c)
 
-##############################################################
+print('С динамическим программированием:', fibanan_dp(11))
+##########################
 
 def fact(n,d):
   if n < 0:
@@ -60,3 +70,12 @@ def FactFirst(n):
   return d
 
 print(FactFirst(5))
+
+def fact_dp(n):
+  d = [0] * n
+  d[0] = 1 # тут значение для 1!
+  for i in range(1,n): # i - индекс в массиве
+    d[i] = d[i-1] * (i+1)
+  return d[n-1]
+
+print(fact_dp(4))
