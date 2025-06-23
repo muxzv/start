79732860c396c744f6f17a3361264a3d483aa952
Mux <96343394+muxzv@users.noreply.github.com>
2025-06-23 16:45:58 +0300

    Update dp_sum.py

--- tinkoff_2024/dp_sum.py
+++ tinkoff_2024/dp_sum.py
@@ -15,6 +15,30 @@ minn = 1000
 task_full_rec([], 0)
 print(minn)
 
+def task_full_rec_helper(l,n,var):
+  if n < 100:
+    task_full_rec_helper(l + [1], n+1,var)
+    task_full_rec_helper(l + [20], n+20,var)
+    task_full_rec_helper(l + [30], n+30,var)
+  elif n == 100:
+    var.append(l)
+
+def task_full_rec2():
+  var = []
+  task_full_rec_helper([], 0,var)
+  #print(len(var))
+  
+  best = None
+  best_l = 101
+  for l in var:
+    if len(l) < best_l:
+      best = l
+      best_l = len(l)
+  
+  return best
+  
+print(task_full_rec2())
+
 # здесь определяется жадность
 def next_item(l,res,n):
   
