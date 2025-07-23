de8c9581268926f7d33b2d6d6f5545f690b40daf
Mux <96343394+muxzv@users.noreply.github.com>
2025-07-23 20:01:27 +0300

    Update River_dp.py

--- tinkoff_2024/River_dp.py
+++ tinkoff_2024/River_dp.py
@@ -1,29 +1,26 @@
 lr = input()
-lr_list = []
+lr_list = [0]
 for s in lr:
   lr_list.append(s)
 
+dp_l = [0] * (len(lr_list))
+dp_r = [0] * (len(lr_list))
 
+dp_r[0] = 1
 
-dp_l = [0] * (len(lr_list) + 1)
-dp_r = [0] * (len(lr_list) + 1)
-
-dp_r[1] = 1
-
-
-for i in range(2,len(lr_list)):
-  print(i)
-  r = min(dp_r[i - 1],dp_l[i] + 1)
-  l = min(dp_l[i - 1],dp_r[i] + 1)
+for i in range(1,len(lr_list)):
+  # print(i,lr_list[i])
   if lr_list[i] == 'L':
-    dp_l[i] = l
-    dp_r[i] = r
+    dp_l[i] = min(dp_l[i - 1] + 1, dp_r[i - 1] + 1)
+    dp_r[i] = min(dp_r[i - 1], dp_l[i - 1] + 1)
   elif lr_list[i] == 'R':
-    dp_l[i] = l
-    dp_r[i] = r + 1
+    dp_l[i] = min(dp_l[i - 1], dp_r[i - 1] + 1)
+    dp_r[i] = min(dp_r[i - 1] + 1, dp_l[i - 1] + 1)
   elif lr_list[i] == 'B':
-    dp_l[i] = l
-    dp_r[i] = r
+    dp_l[i] = min(dp_l[i - 1] + 1, dp_r[i - 1] + 2)
+    dp_r[i] = min(dp_r[i - 1] + 1, dp_l[i - 1] + 2)
     
-print(dp_l)
-print(dp_r)
+#print(dp_l)
+#print(dp_r)
+
+print(dp_r[-1])
