27dc4b19d94ad0cc72c9d29cfd6a040ace1bca9b
Mux <96343394+muxzv@users.noreply.github.com>
2025-08-01 13:30:17 +0300

    Update Count_machine_dp.py

--- tinkoff_2024/Count_machine_dp.py
+++ tinkoff_2024/Count_machine_dp.py
@@ -1,18 +1,18 @@
-a = 20
-b = 22
+a = 5
+b = 10
 
-dp = [0] * b
-dp[a] = 1
+dp = [0] * (b - a)
+dp[0] = 1
 
-for i in range(a+1, b):
+for i in range(1, b - a):
   
   i1 = i - 1
-  if i1 >= a and dp[i1] > 0:
+  if i1 >= 0 and dp[i1] > 0:
     dp[i] += dp[i1]
   
-  if i % 3 == 0:
-    i3 = i // 3
-    if i3 >= a and dp[i3] > 0:
+  if (i+a) % 3 == 0:
+    i3 = (i+a) // 3
+    if i3 >= 0 and dp[i3] > 0:
       dp[i] += dp[i3]
 
 print(dp)
