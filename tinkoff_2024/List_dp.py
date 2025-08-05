bf82e9dce9f20536312ea93fb4aa67240aa4100b
Mux <96343394+muxzv@users.noreply.github.com>
2025-08-05 12:28:57 +0300

    Update List_dp.py

--- tinkoff_2024/List_dp.py
+++ tinkoff_2024/List_dp.py
@@ -1,15 +1,15 @@
-n = int(input())
+num = int(input())
 
-if n == 0 or n == 1:
+if num == 0 or num == 1:
   print(1)
 else:
-  a = [0] * n
+  a = [0] * (num * 3)
   a[0] = 1
   a[1] = 1
-  for i in range(2, n):
+  for i in range(2, num + 1):
+    n = i // 2
     if i % 2 == 0:
-      a[2 * i] = a[i] + 1
+      a[i] = a[n] + 1
     else:
-      a[2 * i + 1] = a[2 * n + 1] - a[i]
-
-print(a)
+      a[i] = a[n] + a[n + 1] + 1
+  print(a[num])
