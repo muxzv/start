f32c1a879c506725d42420183462ec220571b47a
Mux <96343394+muxzv@users.noreply.github.com>
2025-08-06 17:13:46 +0300

    Update Coins_dp.py

--- tinkoff_2024/Coins_dp.py
+++ tinkoff_2024/Coins_dp.py
@@ -1,10 +1,10 @@
-n = 8
+n = int(input())
 
 dp = [0] * (n + 1)
 dp[1] = 1
 
 for i in range(2,n + 1):
     dp[i] = dp[i - 1]
-    if i % 2 == 0 or i % 5 == 0 or i % 10 == 0:
+    if i % 10 % 5 % 2 == 0 or i % 2 == 0 or i % 5 == 0 or i % 10 == 0:
         dp[i] += 1
-print(dp)
+print(dp[-1])
