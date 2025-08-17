19d99604de66110354819b86956e38120d6824a6
Mux <96343394+muxzv@users.noreply.github.com>
2025-08-17 14:48:42 +0300

    Create 1.py

---
+++ ВсОШ/Муницапальный_2024/1.py
@@ -0,0 +1,29 @@
+a = int(input())#математика
+b = int(input())#информатика
+
+k = int(input())
+
+x = int(input())#математика
+y = int(input())#информатика
+
+
+if x == y:
+    print(k * x)
+elif x > y:
+    for _ in range(k + 1):
+        if k > 0:
+            res += 1
+            if x > 0:
+                a -= 1
+            else:
+                b -= 1
+    print(res)
+elif y > x:
+    for _ in range(k + 1):
+        if k > 0:
+            res += 1
+            if b > 0:
+                b -= 1
+            else:
+                a -= 1
+    print(res)
