1e178cb34bd0862b97a98db1413380fa535d955e
Mux <96343394+muxzv@users.noreply.github.com>
2025-08-19 15:49:31 +0300

    Update lmstudio_test.py

--- AI3_Project/lmstudio_test.py
+++ AI3_Project/lmstudio_test.py
@@ -12,7 +12,13 @@ print(model.respond("What is the meaning of life?"))
 image_path = "C:\\tmp\\76.jpg"
 image_handle = lms.prepare_image(image_path)
 
+user_text = "Текст объявления"
+PROMPT = "Я пришлю тебе объявление, а ты должен классифицировать его и в ответе присылать мне подходящие хэштеги и ничего больше." \
+          "Если содержит какой-либо контакт - добавь #spam. Если объявление содержит что-нибудь запрещенное законами РФ, то добавь хэштег #ban." \
+          "Следующим сообщением я пришлю объявление. Дополнительных вопросов не задавай. Объявление:"
+      
+text = PROMPT + user_text
 
 chat = lms.Chat()
-chat.add_user_message("Describe this image please", images=[image_handle])
+chat.add_user_message(text, images=[image_handle])
 prediction = model.respond(chat)
