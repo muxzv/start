6117e00f0e32a165be56f7fef1ef6e048a525f72
Mux <96343394+muxzv@users.noreply.github.com>
2025-08-30 16:18:25 +0300

    Create A1.py

---
+++ ОтборИнформатика/A1.py
@@ -0,0 +1,14 @@
+# Задача 1A. Телефонный набор
+
+# решение Максима, вариант 1
+
+s = input()
+letters = [[],[A,B,C],[D,E,F] ...]
+
+res = 0
+
+for i in range(len(letters)):
+  if e in letters[i]:
+    res += 2 + i - 1
+    
+print(res)
