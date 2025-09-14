e8a51e96d2b2659801496a68a599cd3f8fb8c176
Max Sorvim <m7482844@gmail.com>
2025-09-14 14:38:48 +0300

    etc

--- otbor_tinkoff_2025/1F.py
+++ otbor_tinkoff_2025/1F.py
@@ -1,9 +1,9 @@
 def chet_nechet(n):
     return n % 2 == 0
 
-a = [] # list(map(int,input().split()))
-rows = 4 # a[0]
-cols = 3 # a[1]
+a = list(map(int,input().split()))
+rows = a[0]
+cols = a[1]
 
 m = [[0 for _ in range(cols)] for _ in range(rows)]
 
@@ -22,21 +22,23 @@ for i in range(len(cols_even)):
         ones_cols.append(i)
 
 
-print(ones_cols)
-print(ones_rows)
+#print(ones_cols)
+#print(ones_rows)
 
-n_c = chet_nechet(rows)
-m_c = chet_nechet(cols)
+n_c = chet_nechet(len(ones_rows))
+m_c = chet_nechet(len(ones_cols))
 
 if m_c != n_c:
-    print("MOOOO")
+    print("NO")
 else:
+    print("YES")
     for i in range(max(len(ones_cols),len(ones_rows))):
-        if i <= len(ones_cols):
+        if i < len(ones_cols):
+            #rint(i,len(ones_cols))
             col = ones_cols[i]
-        if i <= len(ones_rows):
+        if i < len(ones_rows):
             row = ones_rows[i]
         m[row][col] = 1
 
-for e in m:
-    print(e)
+    for e in m:
+        print(*e)
