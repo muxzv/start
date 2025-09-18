d93e8760402de4de10f23f7dc63bdb8ed25f6a53
cooffice <55910898+cooffice@users.noreply.github.com>
2025-09-18 10:44:53 +0300

    Update Task7.py

--- ВсОШ/Муницапальный_2024_2/Task7.py
+++ ВсОШ/Муницапальный_2024_2/Task7.py
@@ -1,40 +1,38 @@
 # Задача 7. Задачи на печать!
 
-# решение Максима, вариант 1
+# style - True - 2-сторонная - double
+# style - False - 1-сторонная - single
 
-n = 0
-x = []
-c = 0
-prev_type = None # 1 or 2
-for i in range(n):
-  cur_t = x[i]
-  if prev_type == 2:
-    if cur_t == 1:
-      prev_type = 1
+# 1 = 1
+# 2 = 2
+# 3 = 2+1 = 1+2
+
+def helper(a, start_style):
+  c = 1 # кол-во групп
+  cur_style = start_style
+  need_add = False
+  for e in a:
+    if need_add:
       c += 1
-    if cur_t == 3:
-      prev_type = 1
-      c += 1
-  elif prev_type == None:
-    if cur_t == 1:
-      prev_type = 1
-      c += 1
-    elif cur_t == 2:
-      prev_type = 2
-      c += 1
-    else:
-      if i+1 <= n:
-        c += 1
-        if x[i+1] == 1:
-          prev_type = 1
-        else:
-          prev_type = 2
-      else:
-        break
+      need_add = False
+    if cur_style: # double
+      if e != 2:
+        need_add = True
+        cur_style = False
+    elif e != 1:
+      need_add = True
+      cur_style = True
+  return c
+
+def Task(a):
+  if a[0] == 2:
+    return helper(a, True)
+  if a[0] == 1:
+    return helper(a, False)
   else:
-    if cur_t == 2:
-      prev_type = 2
-      c += 1
-    if cur_t == 3:
-      prev_type = 2
-      c += 1
+    return min(helper(a, True), helper(a, False))
+    
+print(Task([1, 3, 2, 1]))
+print(Task([3, 3]))
+
+print(Task([2, 1, 2, 1]))
