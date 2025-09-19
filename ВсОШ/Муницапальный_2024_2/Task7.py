f71d7cbbc5e13cf199f8b246e16457e746ab4951
Max Sorvim <m7482844@gmail.com>
2025-09-19 15:45:56 +0300

    etc

--- ВсОШ/Муницапальный_2024_2/Task7.py
+++ ВсОШ/Муницапальный_2024_2/Task7.py
@@ -2,39 +2,33 @@
 
 # решение Максима, вариант 1
 
-n = 0
+n = 4 #int(input())
 x = []
-c = 0
-prev_type = None # 1 or 2
-for i in range(n):
-  cur_t = x[i]
-  if prev_type == 2:
-    if cur_t == 1:
-      prev_type = 1
+for _ in range(n):
+    x.append(int(input()))
+mode = 0 # 1 or 2 or 0
+
+
+for e in x:
+    if mode == 0:
+      if e == 1:
+        mode = 1
+      elif e == 2:
+        mode = 2
       c += 1
-    if cur_t == 3:
-      prev_type = 1
-      c += 1
-  elif prev_type == None:
-    if cur_t == 1:
-      prev_type = 1
-      c += 1
-    elif cur_t == 2:
-      prev_type = 2
-      c += 1
-    else:
-      if i+1 <= n:
+
+    if mode == 1:
+      if e == 2:
+        mode = 2
         c += 1
-        if x[i+1] == 1:
-          prev_type = 1
-        else:
-          prev_type = 2
-      else:
-        break
-  else:
-    if cur_t == 2:
-      prev_type = 2
-      c += 1
-    if cur_t == 3:
-      prev_type = 2
-      c += 1
+      elif e == 3:
+        mode = 2
+        c += 1
+    
+    if mode == 2:
+        if e == 1:
+          mode = 0
+        elif e == 3:
+          mode = 0
+
+print(c)
